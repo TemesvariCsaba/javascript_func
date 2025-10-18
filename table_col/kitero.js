e08795c03a60570adeef8873fa8688ecc1277d8a
Temesvari <temesvari.csaba@BOLYAI.LOCAL>
2025-10-18 11:33:27 +0200

    kodrovidites fuggvennyel

--- table_col/kitero.js
+++ table_col/kitero.js
@@ -1,40 +1,7 @@
-console.log("bomba")
-/**
- * @type {string}
- */
-const k = "T1000-es model"
-console.log(k)
-
-const l = ["a", "b"]
-console.log(l[1])
-
-for(let i=0; i<l.length; i++){
-console.log(l[i])
-}
-
-for(const a of l){
-    console.log(a)
-}
-
-for(const key in l){
-    console.log(`${key} : ${l[key]}`) //key+ ":" l[key]
-}
-/**
- * @type {{name: string, age: number}}
- */
-const o = {
-    name: 'csaba',
-    age: '18',
-}
-
-for(const key in o){
-    console.log(`${key} : ${o[key]}`)
-}
-
 /**
  * @type {{name: string, time: string, love1: string, love2: string}[]}
  */
-const arr = [
+const arra = [
     {
         name: 'Balassi Bálint',
         time: 'reformáció',
@@ -70,43 +37,35 @@ table.appendChild(thead)
 const tr = document.createElement('tr') 
 thead.appendChild(tr)
 
-const th = document.createElement('th')
-th.innerText = 'Szerző neve'
-tr.appendChild(th)
 
-const th2 = document.createElement('th') 
-th2.innerText = 'Korszak'
-tr.appendChild(th2)
+createCell('th',"Szerző neve", tr)
 
-const th3 = document.createElement('th')
-th3.innerText = 'Szerelmek'
-tr.appendChild(th3)
-th3.colSpan = 2
+createCell('th',"Korszak", tr)
+
+const fejlec3 = createCell('th',"Szerelmek", tr) 
+fejlec3.colSpan = 2
 
 const tbody = document.createElement('tbody')
 table.appendChild(tbody)
 
 
-for (const i of arr) {
+for (const i of arra) {
     const tr = document.createElement('tr')
     tbody.appendChild(tr)
-    const td = document.createElement('td')
-    td.innerText = i.name
-    tr.appendChild(td)
-    const td2 = document.createElement('td')
-    td2.innerText = i.time
-    tr.appendChild(td2)
-    const td3= document.createElement('td')
-    td3.innerText = i.love1
-    tr.appendChild(td3)
+
+    createCell('td',i.name, tr)
+
+    createCell('td',i.time, tr)
+
+    const td3 = createCell('td',i.love1, tr)
+    
     
     if (i.love2 === undefined){
          td3.colSpan = 2
+
     }
     else{
-    const td4 = document.createElement('td')
-    td4.innerText = i.love2
-    tr.appendChild(td4)
+    createCell('td', i.love2, tr)
     }
 }
 /**
@@ -115,7 +74,7 @@ for (const i of arr) {
  * @param {string} cellType -cella típusa (th, td)
  * @param {string} cellContent -cella tartalma 
  * @param {HTMLTableRowElement} parentRow - appendeljuk a "szulohoz"
- * @returns {HTMLTableCellElement}
+ * @returns {HTMLTableCellElement} - visszatérunk a letrehozott cella elementel
  */
 
 
